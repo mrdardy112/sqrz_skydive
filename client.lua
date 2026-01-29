dc1688ded1696fc80d9ef580413ea0e7e563c54b
ℳℛ.𝒟𝒶𝓇𝒹𝓎 <noah.elias.boeck@gmail.com>
2026-01-29 23:04:46 +0100

    Refactor skydiving exports and commands

--- client.lua
+++ client.lua
@@ -1,33 +1,33 @@
 local playerPed = PlayerPedId()
 
--- Export: Starte Skydiving (aufrufbar von anderen Resources)
+
 exports('StartSkyDive', function(position)
     position = position or vector4(500.0, 5000.0, 1000.0, 0.0)  -- Default über Los Santos
     SetEntityCoords(playerPed, position.x, position.y, position.z, false, false, false, true)
     
     Citizen.Wait(1000)
     
-    TaskSkyDive(playerPed)  -- Native für Skydiving [web:16][web:11]
-    GiveWeaponToPed(playerPed, GetHashKey('GADGET_PARACHUTE'), 1, false, true)  -- Fallschirm geben
+    TaskSkyDive(playerPed)  
+    GiveWeaponToPed(playerPed, GetHashKey('GADGET_PARACHUTE'), 1, false, true)  
     
     TriggerEvent('skydive:notify', 'Skydiving gestartet! Space für Fallschirm.')
 end)
 
--- Export: Deploy Fallschirm sofort
+
 exports('DeployParachute', function()
-    TaskParachuteToTarget(playerPed, 0.0, 0.0, 0.0)  -- Automatisch landen [web:7]
+    TaskParachuteToTarget(playerPed, 0.0, 0.0, 0.0)  
 end)
 
--- Chat-Command für Tests (/skydiving)
+
 RegisterCommand('skydiving', function()
     exports['skydive-legacy']:StartSkyDive()
 end, false)
 
--- Loop für Fallschirm-Deploy mit E-Taste
+
 Citizen.CreateThread(function()
     while true do
         Citizen.Wait(0)
-        if IsControlJustPressed(0, 38) then  -- E-Taste [web:23]
+        if IsControlJustPressed(0, 38) then  
             local parachuteState = GetPedParachuteState(playerPed)
             if parachuteState == 0 then
                 exports['skydive-legacy']:DeployParachute()
@@ -36,7 +36,7 @@ Citizen.CreateThread(function()
     end
 end)
 
--- Event für Notify (intern)
+
 RegisterNetEvent('skydive:notify')
 AddEventHandler('skydive:notify', function(msg)
     BeginTextCommandThefeedPost('STRING')
